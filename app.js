b7651655b12c56d16367fbed69fcab859a707ace
Ben <bwhitfield82@gmail.com>
2019-09-04 22:59:44 +0100

    add not found error and repos information

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ searchUser.addEventListener('keyup', (e) => {
     github.getUser(userText)
     .then(data => {
       if(data.profile.message === 'Not Found') {
-
+        ui.showAlert('user not found', 'alert alert-danger')
       } else {
-        ui.showProfile(data.profile)
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
       }
     })
-  }
-  else {
-
+  } else {
+    ui.clearProfile();
   }
 })
